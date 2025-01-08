c80d7888783c1dc24da112c1c1715b6e74b2a810
Sohaib <sohaib.b.development@gmail.com>
2025-01-08 18:56:02 +0330

    code in browser theme preferance for website's theme
    also moved codes into seperate functions

--- public/javascript/darkmode.js
+++ public/javascript/darkmode.js
@@ -8,48 +8,63 @@ const bgImage = document.getElementById("bg-image");
 const collapseSection = document.getElementById("collapse-section");
 const icons = document.querySelectorAll(".dark-icons");
 const accordionHeading = document.querySelectorAll(".accordion-h");
-darkmodeBtn.addEventListener("click", (event) => {
+function setLight() {
+  html.setAttribute("data-bs-theme", "light");
+  themeIcon.classList.remove("fa-regular");
+  themeIcon.classList.add("fa-solid");
+  nav.classList.remove("darkmode");
+  nav.classList.add("lightmode");
+  bgImage.classList.remove("darkmode");
+  bgImage.classList.add("lightmode");
+  appScreenshotSection.classList.remove("darkmode");
+  appScreenshotSection.classList.add("lightmode");
+  appScreenshot.classList.remove("darkmode");
+  appScreenshot.classList.add("darkmode");
+  collapseSection.classList.remove("darkmode");
+  collapseSection.classList.add("lightmode");
+  icons.forEach((icon) => {
+    icon.classList.remove("darkmode");
+    icon.classList.add("lightmode");
+  });
+  accordionHeading.forEach((h) => {
+    h.classList.remove("darkmode");
+  });
+}
+function setDark() {
+  html.setAttribute("data-bs-theme", "dark");
+  themeIcon.classList.add("fa-regular");
+  themeIcon.classList.remove("fa-solid");
+  nav.classList.add("darkmode");
+  nav.classList.remove("lightmode");
+  bgImage.classList.add("darkmode");
+  bgImage.classList.remove("lightmode");
+  appScreenshotSection.classList.add("darkmode");
+  appScreenshotSection.classList.remove("lightmode");
+  appScreenshot.classList.add("darkmode");
+  appScreenshot.classList.remove("darkmode");
+  collapseSection.classList.add("darkmode");
+  collapseSection.classList.remove("lightmode");
+  icons.forEach((icon) => {
+    icon.classList.add("darkmode");
+    icon.classList.remove("lightmode");
+  });
+  accordionHeading.forEach((h) => {
+    h.classList.add("darkmode");
+  });
+}
+darkmodeBtn.addEventListener("click", () => {
   if (html.getAttribute("data-bs-theme") == "dark") {
-    html.setAttribute("data-bs-theme", "light");
-    themeIcon.classList.remove("fa-regular");
-    themeIcon.classList.add("fa-solid");
-    nav.classList.remove("darkmode");
-    nav.classList.add("lightmode");
-    bgImage.classList.remove("darkmode");
-    bgImage.classList.add("lightmode");
-    appScreenshotSection.classList.remove("darkmode");
-    appScreenshotSection.classList.add("lightmode");
-    appScreenshot.classList.remove("darkmode");
-    appScreenshot.classList.add("darkmode");
-    collapseSection.classList.remove("darkmode");
-    collapseSection.classList.add("lightmode");
-    icons.forEach((icon) => {
-      icon.classList.remove("darkmode");
-      icon.classList.add("lightmode");
-    });
-    accordionHeading.forEach((h) => {
-      h.classList.remove("darkmode");
-    });
+    setLight();
   } else {
-    html.setAttribute("data-bs-theme", "dark");
-    themeIcon.classList.add("fa-regular");
-    themeIcon.classList.remove("fa-solid");
-    nav.classList.add("darkmode");
-    nav.classList.remove("lightmode");
-    bgImage.classList.add("darkmode");
-    bgImage.classList.remove("lightmode");
-    appScreenshotSection.classList.add("darkmode");
-    appScreenshotSection.classList.remove("lightmode");
-    appScreenshot.classList.add("darkmode");
-    appScreenshot.classList.remove("darkmode");
-    collapseSection.classList.add("darkmode");
-    collapseSection.classList.remove("lightmode");
-    icons.forEach((icon) => {
-      icon.classList.add("darkmode");
-      icon.classList.remove("lightmode");
-    });
-    accordionHeading.forEach((h) => {
-      h.classList.add("darkmode");
-    });
+    setDark();
   }
 });
+
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  setDark();
+} else {
+  setLight();
+}
