f914a572f7c3690d87b956f302ede1c616854663
Sohaib <sohaib.b.development@gmail.com>
2025-01-08 18:12:36 +0330

    set up dark mode and light mode

---
+++ public/javascript/darkmode.js
@@ -0,0 +1,50 @@
+const html = document.querySelector("html");
+const nav = document.querySelector("nav");
+const darkmodeBtn = document.getElementById("darkmode-toggle");
+const appScreenshotSection = document.getElementById("app-screenshot-section");
+const appScreenshot = document.getElementById("app-screenshot");
+const bgImage = document.getElementById("bg-image");
+const collapseSection = document.getElementById("collapse-section");
+const icons = document.querySelectorAll(".dark-icons");
+const accordionHeading = document.querySelectorAll(".accordion-h");
+darkmodeBtn.addEventListener("click", (event) => {
+  if (html.getAttribute("data-bs-theme") == "dark") {
+    html.setAttribute("data-bs-theme", "light");
+    nav.classList.remove("darkmode");
+    nav.classList.add("lightmode");
+    bgImage.classList.remove("darkmode");
+    bgImage.classList.add("lightmode");
+    appScreenshotSection.classList.remove("darkmode");
+    appScreenshotSection.classList.add("lightmode");
+    appScreenshot.classList.remove("darkmode");
+    appScreenshot.classList.add("darkmode");
+    collapseSection.classList.remove("darkmode");
+    collapseSection.classList.add("lightmode");
+    icons.forEach((icon) => {
+      icon.classList.remove("darkmode");
+      icon.classList.add("lightmode");
+    });
+    accordionHeading.forEach((h) => {
+      h.classList.remove("darkmode");
+    });
+  } else {
+    html.setAttribute("data-bs-theme", "dark");
+    nav.classList.add("darkmode");
+    nav.classList.remove("lightmode");
+    bgImage.classList.add("darkmode");
+    bgImage.classList.remove("lightmode");
+    appScreenshotSection.classList.add("darkmode");
+    appScreenshotSection.classList.remove("lightmode");
+    appScreenshot.classList.add("darkmode");
+    appScreenshot.classList.remove("darkmode");
+    collapseSection.classList.add("darkmode");
+    collapseSection.classList.remove("lightmode");
+    icons.forEach((icon) => {
+      icon.classList.add("darkmode");
+      icon.classList.remove("lightmode");
+    });
+    accordionHeading.forEach((h) => {
+      h.classList.add("darkmode");
+    });
+  }
+});
